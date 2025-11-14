3f9022f6bcb48c669532977ec656b1d025c29a1f
Khalid <golden-2024@hotmail.com>
2025-11-14 20:13:38 +0000

    Finsished template and volume

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import Features from '../components/Features'
 import About from '../components/About'
 import Footer from '../components/Footer'
+import './HomePage.css'
 
 const HomePage = () => {
   return (
